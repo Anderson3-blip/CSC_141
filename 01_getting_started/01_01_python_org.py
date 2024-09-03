0fa84c21a2150208aa434411c46c8c0bb8d92338
Anderson3-blip <anderson.edouard001@albright.edu>
2024-09-03 08:40:22 -0400

    test

--- 01_getting_started/01_01_python_org.py
+++ 01_getting_started/01_01_python_org.py
@@ -1,2 +1,7 @@
 # Ill be doing this assignment soon
 # I am going to add more code lol
+# one of the things that i found on the website is that you can 
+# use pythong not just to code but to build website GUI development, software
+# development and more, i really the succes story board because people that
+# have use python found that it is flexible and how powerful their ecosystem
+# package is, and having less network unit testing
