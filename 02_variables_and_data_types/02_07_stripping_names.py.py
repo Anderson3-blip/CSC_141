954d41bedd3936dc9046aef8facfeafdc1329b15
Anderson3-blip <anderson.edouard001@albright.edu>
2024-09-05 19:14:35 -0400

    it works

--- 02_variables_and_data_types/02_07_stripping_names.py.py
+++ 02_variables_and_data_types/02_07_stripping_names.py.py
@@ -1,2 +1,9 @@
 tooManySpaces = "         Python Power              "
 print(tooManySpaces.strip)
+
+
+
+name_whitespace = "    Anderson    "
+print(name_whitespace.rstrip())
+print(name_whitespace.lstrip())
+print(name_whitespace.strip())
