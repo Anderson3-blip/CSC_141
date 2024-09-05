decb5ce36b220f3609f05b73ffca8b5816eeaf31
Anderson3-blip <anderson.edouard001@albright.edu>
2024-09-05 08:58:35 -0400

    1st commit

--- 02_variables_and_data_types/02_02_simple_message.py
+++ 02_variables_and_data_types/02_02_simple_message.py
@@ -1,2 +1,5 @@
 message = "hello hopefully i get this soon enough because i want to learn"
-print = (message)
+print (message)
+
+message = "you cant stop just because you want to it is no time yet"
+print (message)
