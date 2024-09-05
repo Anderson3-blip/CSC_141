ec4d09af66256f71be546f2ab3a5630dc6b9310e
Anderson3-blip <anderson.edouard001@albright.edu>
2024-09-05 09:13:59 -0400

    Name new

---
+++ 02_variables_and_data_types/name.py
@@ -0,0 +1,14 @@
+name= "ada lovelace"
+print(name.title())
+
+name= "ada lovelace"
+print(name.upper())
+print(name.lower())
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = "Ada lovelace"
+print(full_name)
+
+print (f"hello ada"),{full_name.title()}
