a8714ff3f0aecd5882dbffca9ed4f9d683e7ff46
Anderson3-blip <anderson.edouard001@albright.edu>
2024-09-10 08:48:13 -0400

    progress

---
+++ 03_introducing_list/bicycles.py
@@ -0,0 +1,26 @@
+bicycles = ['trek,''cannodale','redline','specialized']
+print(bicycles)
+
+
+bicycles = ['trek,''cannodale','redline','specialized']
+print(bicycles[0])
+
+
+
+bicycles = ['trek,''cannodale','redline','specialized']
+print(bicycles[0].title())
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print (bicycles[1])
+print(bicycles[3])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])      
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a redline."
+print(message)            
+
+redline = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {redline[0].title()}."
