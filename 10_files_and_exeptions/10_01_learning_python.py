d7fbf28890cc8bb0ef407d7e07774abe38dda4e0
Anderson3-blip <anderson.edouard001@albright.edu>
2024-11-11 19:00:50 -0500

    commite chapter 10

---
+++ 10_files_and_exeptions/10_01_learning_python.py
@@ -0,0 +1,19 @@
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    content = file.object.read()
+    print(content)
+
+print('___')
+
+with open(filename) as file_object:
+    for line in file_object
+    print(line.rstrip())
+
+print('___')
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
